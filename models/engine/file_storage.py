f4495f68399fa703cca385cc4e54bc3da55c4196
alijr2018 <alirami120@hotmail.fr>
2023-09-20 00:03:12 +0100

    done

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -18,11 +18,13 @@ class FileStorage:
     def all(self, cls=None):
         """Returns a dictionary of models currently in storage"""
         if cls is not None:
-            new_dict = {}
-            for key, value in self.__objects.items():
-                if cls == value.__class__ or cls == value.__class__.__name__:
-                    new_dict[key] = value
-            return new_dict
+            if type(cls) == str:
+                cls = eval(cls)
+            cls_dict = {}
+            for k, v in self.__objects.items():
+                if type(v) == cls:
+                    cls_dict[k] = v
+            return cls_dict
         return self.__objects
 
     def new(self, obj):
@@ -64,7 +66,7 @@ class FileStorage:
 
     def delete(self, obj=None):
         """ delete obj from __objects if it's inside """
-        if obj is not None:
-            key = obj.__class__.__name__ + '.' + obj.id
-            if key in self.__objects:
-                del self.__objects[key]
+        try:
+            del self.__objects["{}.{}".format(type(obj).__name__, obj.id)]
+        except (AttributeError, KeyError):
+            pass
