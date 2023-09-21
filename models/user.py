0ff2483bcb996e2f2d6ff0c4d265d6760b356bb5
alijr2018 <alirami120@hotmail.fr>
2023-09-21 01:21:02 +0100

    done

--- models/user.py
+++ models/user.py
@@ -4,27 +4,13 @@ from models.base_model import BaseModel
 from models.base_model import Base
 from sqlalchemy import Column
 from sqlalchemy import String
-from os import getenv
-import sqlalchemy
-import models
 
 
 class User(BaseModel, Base):
     """This class defines a user by various attributes"""
-    if models.storage_t == 'db':
-        __tablename__ = 'users'
+    __tablename__ = 'users'
 
-        email = Column(String(128), nullable=False)
-        password = Column(String(128), nullable=False)
-        first_name = Column(String(128))
-        last_name = Column(String(128))
-
-    else:
-        email = ""
-        password = ""
-        first_name = ""
-        last_name = ""
-
-    def __init__(self, *args, **kwargs):
-        """initializes user"""
-        super().__init__(*args, **kwargs)
+    email = Column(String(128), nullable=False)
+    password = Column(String(128), nullable=False)
+    first_name = Column(String(128))
+    last_name = Column(String(128))
