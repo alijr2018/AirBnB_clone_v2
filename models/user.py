579ece59bce99e12b38b12ea9a51e292914ed934
alijr2018 <alirami120@hotmail.fr>
2023-09-21 01:25:10 +0100

    done

--- models/user.py
+++ models/user.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """This module defines a class User"""
 from models.base_model import BaseModel
-from models.base_model import Base
-from sqlalchemy import Column
-from sqlalchemy import String
+from sqlalchemy import Column, String
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
