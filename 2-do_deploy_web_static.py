d9f5e7d73ce00bb9ab55c9c1eccc9821fcad1777
alijr2018 <alijr_2018@proton.me>
2023-10-06 23:12:35 +0100

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -11,34 +11,39 @@ env.hosts = ['52.205.94.206', '52.86.161.51']
 
 
 def do_deploy(archive_path):
-    """ Distribute an archive to the web servers and deploy it.
-    True if successful, False otherwise.
-    """
+    """Deploys a web static archive to the web servers"""
     if not exists(archive_path):
         return False
 
     try:
-        put(archive_path, '/tmp/')
+        # Upload the archive to /tmp/ on the web server
+        put(archive_path, "/tmp/")
 
-        file_name = archive_path.split("/")[-1]
-        folder_name = file_name.split(".")[0]
-        release_path = "/data/web_static/releases/{}/".format(folder_name)
-        run("mkdir -p {}".format(release_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, release_path))
+        # Extract the archive to the releases directory
+        archive_filename = archive_path.split("/")[-1]
+        archive_folder = "/data/web_static/releases/{}".format(
+            archive_filename.split(".")[0])
+        run("mkdir -p {}".format(archive_folder))
+        run("tar -xzf /tmp/{} -C {}".format(archive_filename, archive_folder))
 
-        run("rm /tmp/{}".format(file_name))
+        # Delete the archive from the server
+        run("rm /tmp/{}".format(archive_filename))
 
-        run("mv {}web_static/* {}".format(release_path, release_path))
-        run("rm -rf {}web_static".format(release_path))
+        # Move files out of web_static subdirectory
+        run("mv {}/web_static/* {}".format(archive_folder, archive_folder))
 
+        # Remove the now empty web_static subdirectory
+        run("rm -rf {}/web_static".format(archive_folder))
+
+        # Remove the existing symbolic link
         run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(release_path))
 
+        # Create a new symbolic link
+        run("ln -s {} /data/web_static/current".format(archive_folder))
+
+        print("New version deployed!")
         return True
+
     except Exception as e:
+        print("Deployment failed: {}".format(str(e)))
         return False
-
-
-if __name__ == "__main__":
-
-    do_deploy(archive_path)
