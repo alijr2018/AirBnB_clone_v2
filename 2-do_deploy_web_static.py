8bfefb1109a02111a68560af0e5e167f657535e5
alijr2018 <alijr_2018@proton.me>
2023-10-06 22:54:19 +0100

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,48 +1,43 @@
 #!/usr/bin/python3
-"""a Fabric script (based on the file 1-pack_web_static.py),
-that distributes an archive to your web servers,
-using the function do_deploy"""
+"""Deploy an archive of static html to my web servers with Fabric3"""
 
-from fabric.api import env, run, put, local
-from os.path import exists
+from fabric import api
+from fabric.contrib import files
+import os
 
-env.user = 'ubuntu'
-env.hosts = ['52.205.94.206', '52.86.161.51']
+
+api.env.hosts = ['142.44.167.235', '144.217.246.199']
+api.env.user = 'ubuntu'
+api.env.key_filename = '~/.ssh/holberton'
 
 
 def do_deploy(archive_path):
-    """ Distribute an archive to the web servers and deploy it.
-    True if successful, False otherwise.
+    """Function to transfer `archive_path` to web servers.
+
+    Args:
+        archive_path (str): path of the .tgz file to transfer
+
+    Returns: True on success, False otherwise.
     """
-    if not exists(archive_path):
-        print("Archive not found.")
+    if not os.path.isfile(archive_path):
         return False
-
-    try:
-        put(archive_path, '/tmp/')
-
-        file_name = archive_path.split("/")[-1]
-        folder_name = file_name.split(".")[0]
-        release_path = "/data/web_static/releases/{}/".format(folder_name)
-        run("mkdir -p {}".format(release_path))
-        run("tar -xzf /tmp/{} -C {}".format(file_name, release_path))
-
-        run("rm /tmp/{}".format(file_name))
-
-        run("mv {}web_static/* {}".format(release_path, release_path))
-        run("rm -rf {}web_static".format(release_path))
-
-        run("rm -rf /data/web_static/current")
-        run("ln -s {} /data/web_static/current".format(release_path))
-
-        print("New version deployed!")
-        return True
-    except Exception as e:
-        print("Deployment failed: {}".format(str(e)))
-        return False
-
-
-if __name__ == "__main__":
-    archive_path = "versions/web_static_20170315003959.tgz"
-
-    do_deploy(archive_path)
+    with api.cd('/tmp'):
+        basename = os.path.basename(archive_path)
+        root, ext = os.path.splitext(basename)
+        outpath = '/data/web_static/releases/{}'.format(root)
+        try:
+            putpath = api.put(archive_path)
+            if files.exists(outpath):
+                api.run('rm -rdf {}'.format(outpath))
+            api.run('mkdir -p {}'.format(outpath))
+            api.run('tar -xzf {} -C {}'.format(putpath[0], outpath))
+            api.run('rm -f {}'.format(putpath[0]))
+            api.run('mv -u {}/web_static/* {}'.format(outpath, outpath))
+            api.run('rm -rf {}/web_static'.format(outpath))
+            api.run('rm -rf /data/web_static/current')
+            api.run('ln -sf {} /data/web_static/current'.format(outpath))
+            print('New version deployed!')
+        except:
+            return False
+        else:
+            return True
