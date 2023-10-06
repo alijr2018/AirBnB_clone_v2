bb87c32042a862f516d9c5573174f45b19d41865
alijr2018 <alijr_2018@proton.me>
2023-10-06 23:13:44 +0100

    done

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -16,29 +16,22 @@ def do_deploy(archive_path):
         return False
 
     try:
-        # Upload the archive to /tmp/ on the web server
         put(archive_path, "/tmp/")
 
-        # Extract the archive to the releases directory
         archive_filename = archive_path.split("/")[-1]
         archive_folder = "/data/web_static/releases/{}".format(
             archive_filename.split(".")[0])
         run("mkdir -p {}".format(archive_folder))
         run("tar -xzf /tmp/{} -C {}".format(archive_filename, archive_folder))
 
-        # Delete the archive from the server
         run("rm /tmp/{}".format(archive_filename))
 
-        # Move files out of web_static subdirectory
         run("mv {}/web_static/* {}".format(archive_folder, archive_folder))
 
-        # Remove the now empty web_static subdirectory
         run("rm -rf {}/web_static".format(archive_folder))
 
-        # Remove the existing symbolic link
         run("rm -rf /data/web_static/current")
 
-        # Create a new symbolic link
         run("ln -s {} /data/web_static/current".format(archive_folder))
 
         print("New version deployed!")
