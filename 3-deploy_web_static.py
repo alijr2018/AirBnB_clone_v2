1b93704ec9b8aee2392b1d9d49d76f9830bba02c
alijr2018 <alijr_2018@proton.me>
2023-10-06 22:47:44 +0100

    done

--- 3-deploy_web_static.py
+++ 3-deploy_web_static.py
@@ -9,46 +9,63 @@ from datetime import datetime
 from os import makedirs
 
 env.user = 'ubuntu'
-env.hosts = ['52.205.94.206', '52.86.161.51']
+env.hosts = ['<IP_web-01>', '<IP_web-02>']  # Replace with actual IP addresses
 
 
 def do_pack():
-    """ Generate a .tgz archive from the contents of the web_static folder. """
+    """
+    Generate a .tgz archive from the contents of the web_static folder.
+    """
     try:
+        # Create the 'versions' directory if it doesn't exist
         if not exists("versions"):
             makedirs("versions")
 
         now = datetime.now()
         file_name = "web_static_{}.tgz".format(now.strftime("%Y%m%d%H%M%S"))
 
+        # Execute the tar command to create the .tgz archive
         local("tar -cvzf versions/{} web_static".format(file_name))
 
+        # Return the path of the archive
         return "versions/{}".format(file_name)
     except Exception as e:
         return None
 
 
 def do_deploy(archive_path):
-    """ Distribute an archive to the web servers and deploy it.
-    True if successful, False otherwise."""
+    """
+    Distribute an archive to the web servers and deploy it.
+
+    Args:
+        archive_path (str): Path to the archive to be deployed.
+
+    Returns:
+        bool: True if successful, False otherwise.
+    """
     if not exists(archive_path):
         print("Archive not found.")
         return False
 
     try:
+        # Upload the archive to the /tmp/ directory on the server
         put(archive_path, '/tmp/')
 
+        # Extract the archive to the appropriate directory
         file_name = archive_path.split("/")[-1]
         folder_name = file_name.split(".")[0]
         release_path = "/data/web_static/releases/{}/".format(folder_name)
         run("mkdir -p {}".format(release_path))
         run("tar -xzf /tmp/{} -C {}".format(file_name, release_path))
 
+        # Delete the uploaded archive
         run("rm /tmp/{}".format(file_name))
 
+        # Move the contents of the release to the correct location
         run("mv {}web_static/* {}".format(release_path, release_path))
         run("rm -rf {}web_static".format(release_path))
 
+        # Update the symbolic link
         run("rm -rf /data/web_static/current")
         run("ln -s {} /data/web_static/current".format(release_path))
 
@@ -60,13 +77,17 @@ def do_deploy(archive_path):
 
 
 def deploy():
+    # Call do_pack and store the archive path
     archive_path = do_pack()
 
+    # Return False if no archive has been created
     if not archive_path:
         return False
 
+    # Call do_deploy with the new path of the archive
     return do_deploy(archive_path)
 
 
 if __name__ == "__main__":
+    # Execute the deploy function
     deploy()
