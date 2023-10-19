f95e93047d5efbc06329a55a130c0652440df852
alijr2018 <alijr_2018@proton.me>
2023-10-19 12:54:15 +0200

    done

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """a script that starts a Flask web application"""
 
-from flask import Flask
+from flask import Flask, render_template
+from markupsafe import escape
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='templates')
 
 @app.route('/', strict_slashes=False)
 def hello_hbnb():
@@ -23,6 +24,14 @@ def c_text(text):
 def python_text(text):
     text = text.replace('_', ' ')
     return ("Python {}".format(text))
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def number(n):
+    return ("{} is a number".format(n))
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    return (render_template('5-number.html', n=n))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
