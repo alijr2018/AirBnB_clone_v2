d04989a9001af091de834ae537a895a92b882ae0
alijr2018 <alijr_2018@proton.me>
2023-10-20 18:16:54 +0200

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -3,6 +3,8 @@
 
 from flask import Flask
 from flask import render_template
+from models import storage
+from models.state import State
 
 app = Flask(__name__, template_folder='templates')
 
@@ -37,6 +39,18 @@ def number_template(n):
 def number_odd_or_even(n):
     result = "even" if n % 2 == 0 else "odd"
     return (render_template('6-number_odd_or_even.html', n=n, result=result))
+
+@app.route('/states_list', strict_slashes=False)
+def states_list():
+    states = storage.all(State).values()
+    sorted_states = sorted(states, key=lambda x: x.name)
+    return render_template('7-states_list.html', states=sorted_states)
+
+
+@app.teardown_appcontext
+def teardown(exception):
+    storage.close()
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
