0b706d90537651ec51219028db95f4dc98a6f799
alijr2018 <alijr_2018@proton.me>
2023-10-22 08:52:28 +0200

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -10,45 +10,6 @@ from flask import g
 app = Flask(__name__, template_folder='templates')
 
 
-@app.route('/', strict_slashes=False)
-def hello_hbnb():
-    return ("Hello HBNB!")
-
-
-@app.route('/hbnb', strict_slashes=False)
-def hbnb():
-    return ("HBNB")
-
-
-@app.route('/c/<text>', strict_slashes=False)
-def c_text(text):
-    text = text.replace('_', ' ')
-    return ("C {}".format(text))
-
-
-@app.route('/python/', defaults={'text': 'is cool'})
-@app.route('/python/<text>', strict_slashes=False)
-def python_text(text):
-    text = text.replace('_', ' ')
-    return ("Python {}".format(text))
-
-
-@app.route('/number/<int:n>', strict_slashes=False)
-def number(n):
-    return ("{} is a number".format(n))
-
-
-@app.route('/number_template/<int:n>', strict_slashes=False)
-def number_template(n):
-    return (render_template('5-number.html', n=n))
-
-
-@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
-def number_odd_or_even(n):
-    result = "even" if n % 2 == 0 else "odd"
-    return (render_template('6-number_odd_or_even.html', n=n, result=result))
-
-
 @app.route('/states_list', strict_slashes=False)
 def states_list():
     states = storage.all(State).values()
