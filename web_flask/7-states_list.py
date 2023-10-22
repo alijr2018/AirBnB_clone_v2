d9ce8c62c72e2a0b691473d682be8ce1aa6e0e6f
alijr2018 <alijr_2018@proton.me>
2023-10-22 10:20:25 +0200

    done

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -6,8 +6,12 @@ from models import storage
 from models.state import State
 from flask import g
 
+app = Flask(__name__)
 
-app = Flask(__name__, template_folder='templates')
+
+@app.teardown_appcontext
+def teardown_db(error):
+    storage.close()
 
 
 @app.route('/states_list', strict_slashes=False)
@@ -15,11 +19,6 @@ def states_list():
     states = storage.all(State).values()
     states = sorted(states, key=lambda state: state.name)
     return render_template('7-states_list.html', states=states)
-
-
-@app.teardown_appcontext
-def teardown(exception):
-    storage.close()
 
 
 if __name__ == '__main__':
