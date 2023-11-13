559ad605672b5dcb828a38100cab9fd17669d83c
alijr2018 <alijr_2018@proton.me>
2023-11-13 21:41:15 +0100

    done

--- models/state.py
+++ models/state.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 """ State class"""
 
+import models
 from models.base_model import BaseModel, Base
-from sqlalchemy import Column, String
-from sqlalchemy.orm import relationship
+from models.city import City
 from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
